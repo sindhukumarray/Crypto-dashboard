094d888b94dd4051bf8feb5c6cb594ed26d8df99
sindhukumarray <sindhukumarray007@gmail.com>
2026-05-01 17:15:46 +0530

    to fix GitHub Pages path issue resolved

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,17 @@
+// async function loadComponent(id, file) {
+//   const res = await fetch(`../components/${file}`);
+//   const data = await res.text();
+//   document.getElementById(id).innerHTML = data;
+// }
+
+// loadComponent("header", "header.html");
+// loadComponent("footer", "footer.html");
+
 async function loadComponent(id, file) {
-  const res = await fetch(`../components/${file}`);
+  const isSubPage = window.location.pathname.includes("/pages/");
+  const basePath = isSubPage ? "../" : "";
+
+  const res = await fetch(`${basePath}components/${file}`);
   const data = await res.text();
   document.getElementById(id).innerHTML = data;
 }
